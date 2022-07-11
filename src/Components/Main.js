8f152618d6498bb8353bb77be98222e9b9a29b9b
Dheeraj Ahuja <dheeraj.ahuja@sap.com>
2022-07-11 17:34:45 +0530

    React form

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,29 +1,50 @@
 import React from "react";
 import data from "../data";
 export default function Main() {
+  let [meme, setNewImage] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-let [saveState, setNewImage]= React.useState("");
+  let [allMemeImages, memeImage] = React.useState(data);
 
+  const [formData, setFormData] = React.useState({
+    firstName : "",
+    lastName: ""
+  });
 
-function onButtonClick(){
-       const memeArray= data.data.memes;
-       const randomMemeIndex=Math.floor(Math.random() * memeArray.length);
-       const memeUrl=memeArray[randomMemeIndex].url;
-       setNewImage(memeUrl);
-}
+  function getMemeImage() {
+    const memeArray = allMemeImages.data.memes;
+    const randomMemeIndex = Math.floor(Math.random() * memeArray.length);
+    const memeUrl = memeArray[randomMemeIndex].url;
+    setNewImage((prevMeme) => ({
+      ...meme,
+      randomImage: memeUrl,
+    }));
+  }
+
+  function onHandleChange(oEvent) {
+    debugger;
+    console.log(oEvent.target.value);
+  }
 
   return (
     <div className="main-form">
       <div className="input-fields">
-        <input id="input1" placeholder="First text"></input>
+        <input
+          id="input1"
+          placeholder="First text"
+          onChange={onHandleChange}
+        ></input>
         <input id="input2" placeholder="Last text"></input>
       </div>
 
-      <button type="button" onClick={onButtonClick}>
+      <button type="button" onClick={getMemeImage}>
         Get a new meme image 🖼
       </button>
 
-      <img className="meme-img" src={saveState}/>
+      <img className="meme-img" src={meme.randomImage} />
     </div>
   );
 }
